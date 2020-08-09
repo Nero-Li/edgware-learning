653a4db4fd3df0756b604f824e016070d10098fa
Nero_Li <375892799@qq.com>
2020-08-10 01:30:29 +0800

    # eureka集群

---
+++ microservice-discovery-eureka-repli/src/main/java/com/lyming/Application.java
@@ -0,0 +1,19 @@
+package com.lyming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * description:
+ *
+ * @author lyming
+ * @date 2020/8/9 1:34 上午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
